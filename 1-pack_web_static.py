ac80af566448351e1f65e8ad5da8d85508717813
Kabucho G <kabuchogeorge3@gmail.com>
2023-02-07 01:26:32 -0800

    updates

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,31 +5,31 @@ from datetime import datetime
 from fabric.api import local
 
 
-def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    file = "versions/web_static_{}.tgz".format(
-        datetime.now().strftime("%Y%m%d%H%M%S"))
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
-
-# from datetime import datetime
-# import os
-# from fabric.api import local
-
-
 # def do_pack():
-#     """Create an archive"""
-#     zip_name = "web-static_{}.tgz".format(
+#     """Create a tar gzipped archive of the directory web_static."""
+#     file = "versions/web_static_{}.tgz".format(
 #         datetime.now().strftime("%Y%m%d%H%M%S"))
-#     if not os.path.exists("versions"):
-#         local("mkdir -p versions")
-#     z_path = "versions/{}".format(zip_name)
-#     local("tar -czvf {} web_static".format(z_path))
-#     if os.path.exists(z_path):
-#         return z_path
-#     else:
+#     if os.path.isdir("versions") is False:
+#         if local("mkdir -p versions").failed is True:
+#             return None
+#     if local("tar -cvzf {} web_static".format(file)).failed is True:
 #         return None
+#     return file
+
+from datetime import datetime
+import os
+from fabric.api import local
+
+
+def do_pack():
+    """Create an archive"""
+    zip_name = "web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S"))
+    if not os.path.exists("versions"):
+        local("mkdir -p versions")
+    z_path = "versions/{}".format(zip_name)
+    local("tar -czvf {} web_static".format(z_path))
+    if os.path.exists(z_path):
+        return z_path
+    else:
+        return None
