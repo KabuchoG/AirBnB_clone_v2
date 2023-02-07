884892c288323a33144272056c363952cc8be380
Kabucho G <kabuchogeorge3@gmail.com>
2023-02-07 01:22:46 -0800

    updates

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,13 +7,7 @@ from fabric.api import local
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
+    file = "versions/web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S")
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
